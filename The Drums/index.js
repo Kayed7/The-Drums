66a152ce5d9fc63f47e47a40f3366553909c3497
Kayed7 <67514957+Kayed7@users.noreply.github.com>
2022-09-23 23:45:15 +0300

    Update index.js

--- The Drums/index.js
+++ The Drums/index.js
@@ -68,30 +68,3 @@ function makeSound(key) {
 }
 
 
-// function buttonAnimation(currentKey) {
-
-//   var activeButton = document.querySelector("." + currentKey);
- 
-    
-//  activeButton.classList.add("pressed");
-
-//   setTimeout(function() {
-//      activeButton.classList.remove("pressed");
-//    }, 100);
-
-
-// }
-
-
-
-
-  // var _gaq = _gaq || [];
-  // _gaq.push(['_setAccount', 'UA-36251023-1']);
-  // _gaq.push(['_setDomainName', 'jqueryscript.net']);
-  // _gaq.push(['_trackPageview']);
-
-  // (function() {
-  //   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-  //   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-  //   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  // })();
